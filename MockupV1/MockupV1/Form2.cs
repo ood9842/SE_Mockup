eec5afa40367bb65e2ac7e195b749271b1666e69
natthaphon <tam3393@hotmail.co.th>
2018-03-28 15:12:39 +0700

    new read port

--- MockupV1/MockupV1/Form2.cs
+++ MockupV1/MockupV1/Form2.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.IO.Ports;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MockupV1
 {
@@ -21,6 +25,15 @@ namespace MockupV1
         private int addDataCount = 0;
         private int addServerCount = 0;
         private int addFileCount = 0;
+        private SerialPort iSerialPort;
+        private int m_nType = -1;
+
+
+
+
+        byte[] m_btAryBuffer = new byte[4096];
+
+        int m_nLenth = 0;
 
         public Form2()
         {
@@ -81,6 +94,14 @@ namespace MockupV1
             ant6.Text = "ant2 : 00:00:00:00";
             ant7.Text = "ant3 : 00:00:00:00";
             ant8.Text = "ant4 : 00:00:00:00";
+
+
+            comPort.SelectedIndex = 2;
+            comBaudrate.SelectedIndex = 1;
+
+            iSerialPort = new SerialPort();
+
+            iSerialPort.DataReceived += new SerialDataReceivedEventHandler(ReceivedComData);
         }
 
         private void resetAnt()
@@ -97,7 +118,39 @@ namespace MockupV1
 
         private void connectLAN_Click(object sender, EventArgs e)
         {
-            if(!port4.Checked && !port8.Checked)
+
+            string strException = string.Empty;
+            string strComPort = comPort.Text;
+            int nBaudrate = Convert.ToInt32(comBaudrate.Text);
+
+            int nRet = OpenCom(strComPort, nBaudrate, out strException);
+
+            if (nRet != 0)
+            {
+                string strLog = "Connection failed, failure cause: " + strException;
+               
+                MessageBox.Show(strLog);
+              
+
+                return;
+            }
+            else
+            {
+                string strLog = "Connect" + strComPort + "@" + nBaudrate.ToString();
+             
+                MessageBox.Show(strLog);
+                start.Enabled = true;
+                
+                comBaudrate.Enabled = false;
+                comPort.Enabled = false;
+                connectLAN.BackColor = Color.Green;
+                  
+                connectLAN.Enabled = false;
+
+                return;
+            }
+
+            if (!port4.Checked && !port8.Checked)
             {
                 MessageBox.Show("Please Select Port Enable");
             }
@@ -154,6 +207,7 @@ namespace MockupV1
                 start.BackColor = Color.Red;
                 start.ForeColor = Color.White;
                 start.Text = "Stop";
+                label6.Enabled = true;
                 //updateDataGridView();
                 //databasecmd.cmd.CommandText = "SELECT MAX(TIME(time)) as time FROM checkpoint WHERE ant_id = 1";
                 //using (MySqlDataReader reader = databasecmd.cmd.ExecuteReader())
@@ -190,6 +244,7 @@ namespace MockupV1
             }
             else
             {
+                label6.Enabled = false;
                 reset.Enabled = true;
                 start.BackColor = Color.White;
                 start.ForeColor = Color.Black;
@@ -223,5 +278,78 @@ namespace MockupV1
 
         }
 
+        private void ReceivedComData(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            try
+            {
+                int nCount = iSerialPort.BytesToRead;
+                label6.ForeColor = System.Drawing.Color.Green;
+                if (nCount == 0)
+                {
+                    return;
+                }
+
+                byte[] btAryBuffer = new byte[nCount];
+                iSerialPort.Read(btAryBuffer, 0, nCount);
+                
+                Console.Write(ByteArrayToString(btAryBuffer, 0, btAryBuffer.Length));
+                Console.WriteLine("");
+                //RunReceiveDataCallback(btAryBuffer);
+                label6.ForeColor = System.Drawing.Color.Black;
+            }
+            catch (System.Exception ex)
+
+            {
+
+            }
+        }
+        public static string ByteArrayToString(byte[] btAryHex, int nIndex, int nLen)
+        {
+            if (nIndex + nLen > btAryHex.Length)
+            {
+                nLen = btAryHex.Length - nIndex;
+            }
+
+            string strResult = string.Empty;
+
+            for (int nloop = nIndex + 7; nloop < nIndex + nLen - 2; nloop++)
+            {
+                string strTemp = string.Format(" {0:X2}", btAryHex[nloop]);
+
+                strResult += strTemp;
+            }
+
+            return strResult;
+        }
+
+        public int OpenCom(string strPort, int nBaudrate, out string strException)
+        {
+            strException = string.Empty;
+
+            if (iSerialPort.IsOpen)
+            {
+                iSerialPort.Close();
+            }
+
+            try
+            {
+                iSerialPort.PortName = strPort;
+                iSerialPort.BaudRate = nBaudrate;
+                iSerialPort.ReadTimeout = 200;
+                iSerialPort.Open();
+            }
+            catch (System.Exception ex)
+            {
+                strException = ex.Message;
+                return -1;
+            }
+
+            m_nType = 0;
+            return 0;
+        }
+
+        
     }
+
 }
